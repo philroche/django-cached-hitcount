241fd786fb662c8b1a68d03e9ed77c945fa7e3ae
phil.roche <phil.roche@ticket-text.com>
2014-09-01 10:42:29 +0100

    Conditional CSRF Exempt decorator had a bug

--- cached_hitcount/decorators.py
+++ cached_hitcount/decorators.py
@@ -1,8 +1,8 @@
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from cached_hitcount.settings import CACHED_HITCOUNT_CSRF_EXEMPT
 
 def conditional_csrf_exempt(func):
-    if not CACHED_HITCOUNT_CSRF_EXEMPT:
+    if CACHED_HITCOUNT_CSRF_EXEMPT:
         return csrf_exempt(func)
     else:
-        return func
+        return csrf_protect(func)
